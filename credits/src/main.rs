b87fd0eb030df51120b566aca6de8105ce660f0c
baa104 <70290229+baa104@users.noreply.github.com>
2021-10-06 10:02:13 -0400

    Update main.rs
    undid comment-out

--- credits/src/main.rs
+++ credits/src/main.rs
@@ -33,7 +33,7 @@ fn main() {
 	let alvyn = texture_creator.load_texture("images/alvyn_credit_page.png").unwrap();
 	let greg = texture_creator.load_texture("images/greg_credits.jpg").unwrap();
 	let jake = texture_creator.load_texture("images/jake_credits.jpeg").unwrap();
-	//let bryce = texture_creator.load_texture("images/bryce_credits.png").unwrap();
+	let bryce = texture_creator.load_texture("images/bryce_credits.png").unwrap();
 	let austin = texture_creator.load_texture("images/austin_credits.png").unwrap();
 	let evan = texture_creator.load_texture("images/evan_credits.png").unwrap();
 
@@ -69,11 +69,11 @@ fn main() {
 	wincan.present();
 
 	thread::sleep(Duration::from_millis(1500));
-/*
+
 	wincan.copy(&bryce, None, None).ok();
 	wincan.present();
 
-	thread::sleep(Duration::from_millis(1500)); */
+	thread::sleep(Duration::from_millis(1500)); 
 
 	wincan.copy(&austin, None, None).ok();
 	wincan.present();
