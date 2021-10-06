6a48a88823d997dd5751007a6142965374c2778f
Evmo98 <70172720+Evmo98@users.noreply.github.com>
2021-10-05 22:08:05 -0400

    Added Evan's Credit Slide

--- credits/src/main.rs
+++ credits/src/main.rs
@@ -35,7 +35,7 @@ fn main() {
 	let jake = texture_creator.load_texture("images/jake_credits.jpeg").unwrap();
 	//let bryce = texture_creator.load_texture("images/bryce_credits.png").unwrap();
 	//let austin = texture_creator.load_texture("images/austin_credits.png").unwrap();
-	//let evan = texture_creator.load_texture("images/evan_credits.png").unwrap();
+	let evan = texture_creator.load_texture("images/evan_credits.png").unwrap();
 
 	wincan.set_draw_color(Color::RGBA(0, 128, 128, 255));
 	wincan.clear();
@@ -78,10 +78,10 @@ fn main() {
 	wincan.copy(&austin, None, None).ok();
 	wincan.present();
 
-	thread::sleep(Duration::from_millis(1500));
+	thread::sleep(Duration::from_millis(1500));*/
 
 	wincan.copy(&evan, None, None).ok();
 	wincan.present();
-
-	thread::sleep(Duration::from_millis(1500));*/
+	
+	thread::sleep(Duration::from_millis(1500));
 }
