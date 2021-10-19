266b6ff59ffc501963d04764a8af97826054f098
kiraghost <46608234+kiraghost@users.noreply.github.com>
2021-10-19 19:44:40 -0400

    remove sdl core

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,35 @@ mod game;
 mod player;
 mod physics_controller;
 mod rect_collider;
-mod sdl_core;
-
-use crate::sdl_core::SDLCore;
 
 fn main() {
-	let core = SDLCore::init("Warp Wizards", true, 1280, 720).unwrap();
-	let sdl_cxt = &core.sdl_cxt;
+	let sdl_cxt = sdl2::init().unwrap();
 	let video_subsys = sdl_cxt.video().unwrap();
+	let event_pump = sdl_cxt.event_pump().unwrap();
+	
+	let window = video_subsys.window("Warp Wizards", 1280, 720)
+		.build()
+		.map_err(|e| e.to_string())
+		.unwrap();
+
+	let wincan = window.into_canvas().accelerated();
+
+	let wincan = wincan.build()
+		.map_err(|e| e.to_string())
+		.unwrap();
 
 	// current_scene lets the game know which section is running
 	// options: mainmenu, game, credits
 	let mut current_scene = "game";
 
-	if current_scene == "game" {
+	if current_scene == "mainmenu" {
 		//main menu code goes here
 	}
 	if current_scene == "game" {
 		//game code goes here
-		game::show_game(core);
-	} else if current_scene == "credits" {
-		//credits::show_credits(wincan);
+		game::show_game(wincan, event_pump);
+	}
+	else if current_scene == "credits" {
+		credits::show_credits(wincan);
 	}
 }
