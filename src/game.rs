55b58043021a6a8b5f90d82a4a10ce27a2d1dedb
Alvyn Berg <alvynb@pm.me>
2021-10-19 21:59:14 -0400

    Added a fixed version of Jake's changes

--- src/game.rs
+++ src/game.rs
@@ -12,12 +12,15 @@ use crate::player::player::Player;
 use crate::physics_controller::physics_controller::PhysicsController;
 use crate::rect_collider::rect_collider::RectCollider;
 
+const TILE_SIZE: u32 = 64;
+
 pub(crate) fn show_game(mut wincan: WindowCanvas, mut event_pump: sdl2::EventPump) -> Result<(), String> {
     let texture_creator = wincan.texture_creator();
 
     let frame_rate = 60;
 
     let p1sprite = texture_creator.load_texture("assets/sprite_sheets/characters-sprites.png").unwrap();
+    let stone_sprite = texture_creator.load_texture("assets/single_assets/stone_brick_64x64.png").unwrap();
     let p1physcon = PhysicsController::new("player1".to_string(), 0.0, 0.0, 6.0, 0.7, 20.0, 1, 0.2, 1.0, 50.0);
     let p1collider = RectCollider::new(0.0, 0.0, 100.0, 100.0, 0.0, true);
 
@@ -27,14 +30,6 @@ pub(crate) fn show_game(mut wincan: WindowCanvas, mut event_pump: sdl2::EventPum
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => break 'gameloop,
-                /*Event::KeyDown{keycode: Some(k), ..} => {
-                    match k {
-                        Keycode::W => player1.physics.jump(),
-                        Keycode::A => player1.physics.accelerate_left(),
-                        Keycode::D => player1.physics.accelerate_right(),
-                        _ => {},
-                    }
-                } */
                 _ => {},
             }
         }
@@ -54,17 +49,28 @@ pub(crate) fn show_game(mut wincan: WindowCanvas, mut event_pump: sdl2::EventPum
             player1.physics.jump();
         }
 
-        if player1.physics.y() > 620.0 && player1.physics.fall_speed() > 0.0 {
+        if player1.physics.y() > 550.0 && player1.physics.fall_speed() > 0.0 {
             player1.physics.set_grounded();
             player1.physics.reset_jumps();
             player1.physics.set_y(player1.physics.y() - player1.physics.fall_speed());
         }
 
-        player1.physics.update();
-        //player1.physics.debug();
-
         wincan.set_draw_color(Color::RGBA(0, 128, 128, 255));
         wincan.clear();
+
+        // Draw stone
+        let mut i = 0;
+        while i < 130 {
+            let src = Rect::new(((i % 4) * TILE_SIZE) as i32, 0, TILE_SIZE, TILE_SIZE);
+            let pos = Rect::new((i * 10) as i32, (720 - TILE_SIZE) as i32, TILE_SIZE, TILE_SIZE);
+
+            wincan.copy(&stone_sprite, src, pos).ok();
+
+            i += 1;
+        }
+
+        player1.physics.update();
+        //player1.physics.debug();
 
         wincan.copy(&player1.sprite_sheet, Rect::new(100, 0, 100, 100), Rect::new(player1.physics.x() as i32, player1.physics.y() as i32, 100, 100)).ok();
         wincan.present();
@@ -74,5 +80,5 @@ pub(crate) fn show_game(mut wincan: WindowCanvas, mut event_pump: sdl2::EventPum
     }
 
     // Out of game loop, return Ok
-	Ok(())
+    Ok(())
 }
