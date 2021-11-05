7b530a1f0e415421b9a7f9fe0b5e48f540956440
Alvyn Berg <alvynb@pm.me>
2021-11-05 16:27:46 -0400

    Added known_bugs.md; changed menu.png to start_screen.png; refactored main.rs; somewhat cleaned up gif-character-orange.gif

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate sdl2;
 
 use std::env;
+use sdl2::mouse::MouseUtil;
+use sdl2::render::WindowCanvas;
+use sdl2::{EventPump, Sdl};
 
 mod credits;
 mod game;
@@ -16,22 +19,14 @@ mod object_controller;
 mod portal_traversible;
 
 fn main() {
-	let sdl_cxt = sdl2::init().unwrap();
-	let video_subsys = sdl_cxt.video().unwrap();
+	let (sdl_cxt, wincan) = setup_sdl_and_window();
 	let event_pump = sdl_cxt.event_pump().unwrap();
 	let mouse = sdl_cxt.mouse();
-	
-	let window = video_subsys.window("Warp Wizards", 1280, 720)
-		.build()
-		.map_err(|e| e.to_string())
-		.unwrap();
 
-	let wincan = window.into_canvas().accelerated();
+	perform_start_logic(wincan, event_pump, mouse)
+}
 
-	let wincan = wincan.build()
-		.map_err(|e| e.to_string())
-		.unwrap();
-
+fn perform_start_logic(wincan: WindowCanvas, event_pump: EventPump, mouse: MouseUtil) {
 	let args: Vec<String> = env::args().collect();
 	let mut mode = networking::NetworkingMode::Send;
 	if args.len() == 2 {
@@ -44,6 +39,18 @@ fn main() {
 	} else {
 		menu::show_menu(wincan, event_pump, mouse, mode);
 	}
+}
 
-
+fn setup_sdl_and_window() -> (Sdl, WindowCanvas) {
+	let sdl_cxt = sdl2::init().unwrap();
+	let video_subsys = sdl_cxt.video().unwrap();
+	let window = video_subsys.window("Warp Wizards", 1280, 720)
+		.build()
+		.map_err(|e| e.to_string())
+		.unwrap();
+	let wincan = window.into_canvas().accelerated();
+	let wincan = wincan.build()
+		.map_err(|e| e.to_string())
+		.unwrap();
+	(sdl_cxt, wincan)
 }
